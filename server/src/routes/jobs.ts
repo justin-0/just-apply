c1521c337386ae1b7529646f014a7a7f3ad9fc20
justin-0 <itsjustinoconnell@gmail.com>
2024-09-06 10:31:11 +0100

    zod, hono/zod-validator - job post endpoint form data is validated before inserting to db

--- server/src/routes/jobs.ts
+++ server/src/routes/jobs.ts
@@ -1,25 +1,37 @@
 import { Hono } from "hono";
 import { db } from "../lib/prisma";
 import { Context } from "../lib/context";
+import { zValidator } from "@hono/zod-validator";
+import { z, ZodError } from "zod";
+
+export const jobSchema = z.object({
+  role: z.string().min(1, { message: "Role is required" }),
+  company: z.string().min(1, { message: "Company is required" }),
+  status: z.enum(["Applied", "Rejected", "Accepted"], {
+    message: "Status is not valid",
+  }),
+});
 
 export const jobsRouter = new Hono<Context>();
 
-jobsRouter.post("/job", async (c) => {
-  const body = await c.req.json();
+jobsRouter.post("/job", zValidator("json", jobSchema), async (c) => {
   const user = c.get("user");
-
   if (!user) {
     return c.json({ message: "Must be signed in, to create job" }, 401);
   }
+  const result = c.req.valid("json");
 
-  const post = await db.job.create({
-    data: {
-      role: body.role,
-      company: body.company,
-      status: body.status,
-      userId: user.id,
-    },
-  });
-
-  return c.json({ success: true, post }, 201);
+  try {
+    const post = await db.job.create({
+      data: {
+        role: result.role,
+        company: result.company,
+        status: result.status,
+        userId: user.id,
+      },
+    });
+    return c.json({ success: true, post }, 201);
+  } catch (e) {
+    console.log(e);
+  }
 });

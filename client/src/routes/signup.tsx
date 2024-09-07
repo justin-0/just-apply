8303dc1916a6271ea4484d1935b0a4a3f9fa09fa
justin-0 <itsjustinoconnell@gmail.com>
2024-09-07 16:18:56 +0100

    client - components added to build forms

--- client/src/routes/signup.tsx
+++ client/src/routes/signup.tsx
@@ -1,10 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { client } from "../lib/api";
 
 export const Route = createFileRoute("/signup")({
   component: Signup,
 });
 
 function Signup() {
-  return <div>signup</div>;
+  return <div></div>;
 }
